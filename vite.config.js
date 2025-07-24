912a2ab0c3067e1c8f1c82358686ed6665e68d1f
Glyzza Lei <92197857+Psyl0ck3@users.noreply.github.com>
2025-07-25 00:03:38 +0800

    fix routing and rebuild

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
-// vite.config.js
+import { fileURLToPath } from 'url';
+import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   base: '/my-portfolio/',
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve('./src'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
